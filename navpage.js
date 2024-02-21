ad190cf85ad6f0aa2f3810ec0ab7e646f4ee4493
Imran <mohdimranshountoo@gmail.com>
2024-02-21 13:03:56 +0530

    updated code

--- navpage.js
+++ navpage.js
@@ -1,5 +1,59 @@
-function changeContent(param1,param2) {
-    document.getElementById(param2).style.backgroundColor="blue";
-    document.getElementById('content').innerHTML= document.getElementById(param1).innerHTML;
+let activeButton = null;
+
+function changeContent(content, buttonId) {
+    if (activeButton) {
+        document.getElementById(activeButton).classList.remove("myStyle");
+    }
+
+    const list= document.getElementById(buttonId).classList;
+    list.add("myStyle");
+
+    activeButton = buttonId;
+
+    document.getElementById('content').innerHTML = document.getElementById(content).innerHTML;
+    if (buttonId==="bar5"){
+
+        
+            weatherBallon( 3245 );
+        
+    }
+
+}
+
+const key = '50c2bc48e2ae5b37bf4637a5d16d2377';
+if(key=='') document.getElementById('temp').innerHTML = ('');
+
+
+
+function weatherBallon( cityID ) {
+	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+	.then(function(resp) { return resp.json() }) // Convert data to json
+	.then(function(data) {  //check for await and async
     
+    console.log(data);
+		drawWeather(data);
+	})
+	.catch(function() {
+		// catch any errors
+	});
+  console.log("error")
+}
+function drawWeather( d ) {
+  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+
+  var description = d.weather[0].description; 
+	
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+  
+  if( description.indexOf('rain') > 0 ) {
+  	document.body.className = 'rainy';
+  } else if( description.indexOf('cloud') > 0 ) {
+  	document.body.className = 'cloudy';
+  } else if( description.indexOf('sunny') > 0 ) {
+  	document.body.className = 'sunny';
+  } else {
+  	document.body.className = 'clear';
+  }
 }
